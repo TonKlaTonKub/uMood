86631e2f354a428e576edc0ed993ce522f4ae89d
Phetkla Bunnikool <phetkla1992@gmail.com>
2021-05-19 19:46:35 +0700

    Demo1 Main Screen

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import ReactNavigator from './navigation/ReactNavigator'
+import Main from './screen/Main'
 
 
 const App = () => {
-  return <ReactNavigator/>
+  //return <ReactNavigator/>
+  return <Main/>
 }
 
 export default App
--- screen/Main.js
+++ screen/Main.js
@@ -1,34 +1,76 @@
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
 
-
-const Main = () => {
-    return (
-        <ImageBackground source={require('../assets/bg.jpg')} style={styles.container}>
-            <View style={styles.statsContainer}>
-                <Image 
-                    style={styles.sizeImage}
-                    source={require('../assets/Profile_Image.jpg')}
-                />
-                <Text style={styles.marginLeft}>
-                    ( Your Name )
-                </Text>
-            </View>
-            <View style={styles.Layout_Text}>
-                <Text style={styles.Text_Month}>
-                    May 2021
-                </Text>
-            </View>
-            <View style={styles.Layout_Text}>
-                <Image 
-                    source={require('../assets/circle_bg.png')} 
-                    style={styles.test_image}
-                />
-                <Text>Test</Text>
-            </View>
-        </ImageBackground>
-    )
+class Main extends React.Component {
+    render() {
+        return (
+            <ImageBackground source={require('../assets/bg.jpg')} style={styles.container}>
+                <View style={styles.statsContainer}>
+                    <Image 
+                        style={styles.sizeImage}
+                        source={require('../assets/icon/Pic_profile.png')}
+                    />
+                    <Text style={styles.marginLeft}>
+                        ( Your Name )
+                    </Text>
+                </View>
+                <View style={styles.Layout_Text}>
+                    <Text style={styles.Text_Month}>
+                        May 2021
+                    </Text>
+                </View>
+                <View style={styles.Layout_Text}>
+                    <Image 
+                        source={require('../assets/Circle_bg.png')}
+                        style={styles.test_image}
+                    />
+                    <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+                        <Text style={{fontSize: 30}}>
+                            Tuesday
+                        </Text>
+                        <Text style={{fontSize: 50}}>
+                            11
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.statsContainer2}>
+                    <Image 
+                        style={styles.sizeImage}
+                        source={require('../assets/icon/Mood_Happy.png')}
+                    />
+                    <Text style={styles.marginLeft}>
+                        Happy
+                    </Text>
+                    <Text style={styles.marginLeft, {fontSize: 40, marginLeft: 30, marginTop: 7}}>
+                        +
+                    </Text>
+                </View>
+                <View style={styles.statsContainer, {marginLeft: 20, marginTop: 10}}>
+                    <TextInput
+                        label="... Memo ..."
+                        style={{fontSize: 20, width: 355, height: 150}}
+                    />
+                </View>
+                <View style={styles.statsContainer}>
+                    <TextInput
+                        label="+"
+                        style={{fontSize: 20, width: 112, height: 100, marginTop: 5}}
+                    />
+                    <TextInput
+                        label="+"
+                        style={{fontSize: 20, width: 112, height: 100, marginTop: 5, marginLeft: 10}}
+                    />
+                    <TextInput
+                        label="+"
+                        style={{fontSize: 20, width: 112, height: 100, marginTop: 5, marginLeft: 10}}
+                    />
+                    </View>
+            </ImageBackground>
+        )
+    }
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -41,8 +83,14 @@ const styles = StyleSheet.create({
     },
     statsContainer: {
         flexDirection: "row",
-        marginTop: 30,
+        marginTop: 10,
         marginLeft: 20
+    },
+    statsContainer2: {
+        flexDirection: "row",
+        marginLeft: 20,
+        backgroundColor: '#FFF',
+        marginRight: 180
     },
     marginLeft: {
         marginLeft: 20,
@@ -54,11 +102,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFA07A'
     },
     Layout_Text: {
-        alignItems: 'center'
+        alignItems: 'center',
     },
     test_image: {
-        width: 300,
-        height: 300,
+        width: 250,
+        height: 250,
         alignItems: 'center'
     }
   });
